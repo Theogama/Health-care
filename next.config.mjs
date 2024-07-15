700988ebc91e7edd8fbe46f067bb5999db5affae
Theogama <sandilegama.sg@gmail.com>
2024-07-15 13:19:39 +0200

    modify org

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ export default withSentryConfig(withSentryConfig(withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "creativegings",
+org: "javascript-mastery",
 project: "care-pulse",
 
 // Only print logs for uploading source maps in CI
